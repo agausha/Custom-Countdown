2045f212c727415ae0d4716a521b09e069a69c0a
Isaac <uniqucode@gmail.com>
2022-09-11 07:14:47 +0100

    Refrencing countdownElDateError

--- js/script.js
+++ js/script.js
@@ -11,3 +11,4 @@ const CompleteEl = document.getElementById('complete');
 const completeElInfo = document.getElementById('complete-info');
 const completeBtn = document.getElementById('complete-button');
 const countdownElTitleError = document.querySelector('.countdown-title-error');
+const countdownElDateError = document.querySelector('.countdown-date-error');
