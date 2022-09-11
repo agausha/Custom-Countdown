edc9ff898e90fe85e1f1271a5e415696ec5e1644
Isaac <uniqucode@gmail.com>
2022-09-11 07:09:00 +0100

    Refrencing CompleteEl

--- js/script.js
+++ js/script.js
@@ -6,3 +6,5 @@ const countdownEl = document.getElementById('countdown');
 const countdownElTitle = document.getElementById('countdown-title');
 const countdownBtn = document.getElementById('countdown-button');
 const timeElements = document.querySelectorAll('span');
+
+const CompleteEl = document.getElementById('complete');
